2c9c0b05f8268e0d7e414c63124c97c5e8fa6d21
tduarte29 <t.duarte@aluno.ifsp.edu.br>
2023-09-25 00:32:59 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import { useState } from 'react';
+import Tictactoe from './Components/tictactoe';
+
+function App() {
+return(
+<div>
+  <Tictactoe/>
+</div>
+
+
+  )
+}
+
+export default App;
+
+
