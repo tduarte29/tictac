e91528535b03bf02a96337bf2500be48625a7455
tduarte29 <129213483+tduarte29@users.noreply.github.com>
2023-10-09 14:55:26 -0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import './App.css';
-import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Tictactoe from './Components/tictactoe';
+import OutraPagina from './Components/OutraPagina';
 
 function App() {
-return(
-<div>
-  <Tictactoe/>
-</div>
-
-
-  )
+    return (
+        <Router>
+          <Routes>
+                <Route path="/" exact element={<Tictactoe/>} />
+                <Route path="/outra-pagina" element={<OutraPagina/>} />
+          </Routes>
+        </Router>
+    );
 }
 
 export default App;
-
-
