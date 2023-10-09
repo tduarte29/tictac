6e2d35b39ea54cf0819520d66175c5efc250530e
tduarte29 <129213483+tduarte29@users.noreply.github.com>
2023-10-09 15:45:37 -0300

    api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+// chamando a api e importando biblioteca axios por ser mais simples do que fetch nesse cenario
 export const api = axios.create({
     baseURL: 'https://digimon-api.vercel.app/api/'
 });
